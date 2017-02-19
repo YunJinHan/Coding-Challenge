0d89bf3e0d7b03b318b38503a73e57d1a0c4b473
YunJinHan <yjh9234@gmail.com>
2017-02-19 23:07:12 +0900

    Snake Game

---
+++ SnakeGame/sketch.js
@@ -0,0 +1,45 @@
+var s;
+var scl = 20;
+var apple;
+
+function setup() {
+    var cnv = createCanvas(600,600);
+    cnv.position(100,100);
+    s = new Snake();
+    frameRate(10);
+    pickLocation();
+}
+
+function pickLocation() {
+    var cols = floor(width/scl);
+    var rows = floor(height/scl);
+    apple = createVector(floor(random(cols)), floor(random(rows)));
+    apple.mult(scl);
+}
+
+function draw() {
+    background(51);
+
+    if (s.eat(apple)) {
+        pickLocation();
+    }
+
+    s.gameOver();
+    s.update();
+    s.show();
+
+    fill(255,0,100);
+    rect(apple.x, apple.y, scl, scl);
+}
+
+function keyPressed() {
+    if (keyCode == UP_ARROW) {
+        s.dir(0,-1);
+    } else if (keyCode == DOWN_ARROW) {
+        s.dir(0,1);
+    } else if (keyCode == RIGHT_ARROW) {
+        s.dir(1,0);
+    } else if (keyCode == LEFT_ARROW) {
+        s.dir(-1,0);
+    }
+}
---
+++ SnakeGame/snake.js
@@ -0,0 +1,60 @@
+function Snake() {
+    this.x = 0;
+    this.y = 0;
+    this.xspeed = 1;
+    this.yspeed = 0;
+    this.totalLength = 0;
+    this.tail = [];
+
+    this.eat = function(position) {
+        var d = dist(this.x, this.y, position.x, position.y);
+        if (d < 1) {
+            this.totalLength ++;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    this.dir = function(x,y) {
+        this.xspeed = x;
+        this.yspeed = y;
+    }
+
+    this.gameOver = function() {
+        for (var i = 0; i < this.tail.length; i ++) {
+            var pos = this.tail[i];
+            var d = dist(this.x, this.y, pos.x, pos.y);
+            if (d < 1) {
+                this.totalLength = 0;
+                this.tail = [];
+            }
+        }
+    }
+
+    this.update = function () {
+        if (this.totalLength === this.tail.length) {
+            for (var i = 0; i < this.tail.length - 1; i ++) {
+                this.tail[i] = this.tail[i + 1];
+            }
+        }
+        this.tail[this.totalLength - 1] = createVector(this.x, this.y);
+
+        this.x = this.x + this.xspeed*scl;
+        this.y = this.y + this.yspeed*scl;
+
+        this.x = constrain(this.x, 0, width-scl);
+        this.y = constrain(this.y, 0, height-scl);
+    }
+
+    this.show = function () {
+        fill(255);
+        for (var i = 0; i < this.tail.length; i ++) {
+            rect(this.tail[i].x, this.tail[i].y, scl, scl);
+        }
+
+        fill(255);
+        rect(this.x, this.y, scl, scl);
+    }
+
+}
