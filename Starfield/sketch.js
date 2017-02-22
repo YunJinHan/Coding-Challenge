c8fbc8c87730f6a76661c0fa9ee99492c96c79c9
YunJinHan <yjh9234@gmail.com>
2017-02-23 00:25:39 +0900

    Starfield

--- Starfield/sketch.js
+++ Starfield/sketch.js
@@ -5,13 +5,18 @@ var state = document.getElementById("state");
 function setup() {
     var cnv = createCanvas(600,600);
     cnv.parent("main");
-    state.innerText = "Mouse Left Move : Star Stop\nMouse Right Move : Star Move";
+    state.innerText = "Mouse UP Move : Star Move Slow\nMouse DOWN Move : Star Move Fast";
     for (var i = 0; i < 500; i ++) {
         star[i] = new Star();
     }
 }
 
 function draw() {
+    speed = map(mouseY, 0, height, 1, 20);
     background(0);
     translate(width/2,height/2);
+    for (var i = 0; i < star.length; i ++) {
+        star[i].update();
+        star[i].show();
+    }
 }
--- Starfield/star.js
+++ Starfield/star.js
@@ -3,5 +3,33 @@ function Star() {
     this.x = random(-width,width);
     this.y = random(-height,height);
     this.z = random(width);
-    
+    this.zpos = this.z;
+
+    this.update = function() {
+        this.z = this.z - speed;
+        if (this.z < 1) {
+            this.z = width;
+            this.x = random(-width,width);
+            this.y = random(-height,height);
+            this.zpos = this.z;
+        }
+    }
+
+    this.show = function() {
+        fill(255);
+        noStroke();
+
+        var xspeed = map(this.x / this.z, 0, 1, 0, width);
+        var yspeed = map(this.y / this.z, 0, 1, 0, height);
+        var radius = map(this.z, 0, width, 16, 0);
+        ellipse(xspeed, yspeed, radius, radius);
+
+        var xpos = map(this.x / this.zpos, 0, 1, 0, width);
+        var ypos = map(this.y / this.zpos, 0, 1, 0, height);
+
+        this.zpos = this.z;
+
+        stroke(255);
+        line(xpos, ypos, xspeed, yspeed);
+    }
 }
