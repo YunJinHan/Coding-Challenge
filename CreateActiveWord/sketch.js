a0404d4a8e92362240585a0cf6200daf3b5b370b
YunJinHan <yjh9234@gmail.com>
2017-02-25 00:38:06 +0900

    Create Active Word

--- CreateActiveWord/sketch.js
+++ CreateActiveWord/sketch.js
@@ -1,28 +1,48 @@
 var font;
-var words = [];
+var first = true;
+var words;
+var create = document.getElementById("button");
 
 function preload() {
     font = loadFont('libraries/THE_Oegyeinseolmyeongseo.ttf');
+    create.onclick = setup;
 }
 
 function setup() {
-    var cnv = createCanvas(600,300);
-    cnv.parent('main');
-    var points = font.textToPoints('JinHan',30,200,192);
-    console.log(points);
-
+    words = [];
+    var inputText = document.getElementById("input");
+    var text = inputText.value;
+    if (inputText.value == "" || inputText.value== null || typeof inputText.value == undefined) {
+        text = "JinHan";
+    }
+    inputText.value = "";
+    if (first === true) {
+        var cnv = createCanvas(600,300);
+        cnv.parent('main');
+    }
+    var points = font.textToPoints(text,20,200,192);
+    var maxX = 0;
     for (var i = 0; i < points.length; i ++) {
         var pt = points[i];
         var word = new Word(pt.x,pt.y);
+        if (pt.x > maxX) {
+            maxX = pt.x;
+        }
         words.push(word);
     }
-
+    if (first === false) {
+        var canvasWidth = pt.x + 80;
+        var cnv = createCanvas(canvasWidth,300);
+        cnv.parent('main');
+    }
+    first = false;
 }
 
 function draw() {
     background(51);
     for (var i = 0; i < words.length; i ++) {
         var w = words[i];
+        w.behaviors();
         w.update();
         w.show();
     }
--- CreateActiveWord/word.js
+++ CreateActiveWord/word.js
@@ -1,18 +1,64 @@
 function Word(x,y) {
-    this.pos = createVector(x,y);
+    this.pos = createVector(random(width),random(height));
     this.target = createVector(x,y);
     this.vel = p5.Vector.random2D();
     this.acc = createVector();
     this.r = 8;
+    this.maxSpeed = 8;
+    this.maxForce = 0.5;
+}
+
+Word.prototype.behaviors = function() {
+    var arrive = this.arrive(this.target);
+    var mouse = createVector(mouseX,mouseY);
+    var flee = this.flee(mouse);
+
+    arrive.mult(1);
+    flee.mult(2);
+
+    this.applyForce(arrive);
+    this.applyForce(flee);
+}
+
+Word.prototype.applyForce = function(force) {
+    this.acc.add(force);
 }
 
 Word.prototype.update = function() {
     this.pos.add(this.vel);
     this.vel.add(this.acc);
+    this.acc.mult(0);
 }
 
 Word.prototype.show = function() {
     stroke(255);
     strokeWeight(8);
     point(this.pos.x,this.pos.y);
+}
+
+Word.prototype.arrive = function(target) {
+    var desired = p5.Vector.sub(target, this.pos);
+    var distance = desired.mag();
+    var speed = this.maxSpeed;
+    if (distance < 100) {
+        speed = map(distance, 0, 100, 0, this.maxSpeed);
+    }
+    desired.setMag(speed);
+    var steer = p5.Vector.sub(desired, this.vel);
+    steer.limit(this.maxForce);
+    return steer;
+}
+
+Word.prototype.flee = function(target) {
+    var desired = p5.Vector.sub(target, this.pos);
+    var distance = desired.mag();
+    if (distance < 50) {
+        desired.setMag(this.maxSpeed);
+        desired.mult(-1);
+        var steer = p5.Vector.sub(desired, this.vel);
+        steer.limit(this.maxForce);
+        steer.mult(-1);
+        return steer;
+    }
+    return createVector(0,0);
 }
